4f0a2878583fb37f5c865d7abacbc9af2b7017b7
Valeria Pacheco <vpachecosanmartin@gmail.com>
2024-06-13 07:28:48 -0500

    tipado primitivo

---
+++ src/variables-types.ts
@@ -0,0 +1,22 @@
+let bookTitle: string = "TypeScript Basics";
+const libraryName: string = "My Library";
+let numberOfBooks: number = 10;
+let isLibraryOpen: boolean = true;
+
+
+let titulo: "La Odisea"
+let autor: "Homero"
+let genero: "Épica"
+let idioma: "Griego"
+let precio: 23.00
+let formato: "Tapa dura"
+let isbn: "978-0-14-026886-7"
+let descripcion: "Una epopeya de aventuras."
+let estado: "Nuevo"
+let ubicacion: "Atenas"
+let fecha_publicacion: "-800-01-01"
+let editorial: "Editorial Sudamericana"
+let paginas: 541
+let dimensiones: "15x3x23 cm"
+let peso: "0.7 kg"
+let disponible: "si"
